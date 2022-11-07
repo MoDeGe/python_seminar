21be6fe930c0988457856db904e65e5f6e7a3072
Денис Мохов <mokhov.denis@gmail.com>
2022-11-07 14:03:57 +0300

    add 08

--- python_seminar_08/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-
-logging.basicConfig(
-    level=logging.INFO,
-    filename='python_seminar_08\employees.log',
-    format='[%(asctime)s] [%(levelname)s] [%(module)s] [%(funcName)s: %(lineno)d] => %(message)s',
-    datefmt='%d.%m.%Y %H:%M:%S ',
-)
